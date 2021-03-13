c60f59eb719a43948c9478d1538c6943d60fbee8
Ben Lambert <46206905+benlambert43@users.noreply.github.com>
2021-03-13 14:05:18 -0500

    Hi and Echo

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 // Run dotenv
-require('dotenv').config();
+require("dotenv").config();
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 client.login(process.env.DISCORD_TOKEN);
 
+const prefix = "!";
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(" ");
+  const command = args.shift().toLowerCase();
 
-client.on('message', message => {
-    // If the message is "ping"
-    if (message.content === 'ping') {
-      // Send "pong" to the same channel
-      message.channel.send('pong');
+  if (command === "hi") {
+    return message.channel.send(`Hi ${message.author}, I'm frenlyBot!`);
+  } else if (command === "echo") {
+    if (!args.length) {
+      return message.channel.send(
+        `You didn't provide any arguments, ${message.author}!`
+      );
     }
-  });
+    message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+  }
+});
